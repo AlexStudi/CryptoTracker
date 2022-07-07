8b8db34c04b141c3205defe1813ebb961398c114
Alexandre Pedarriosse <alexandre.pedarriosse@gmail.com>
2022-07-08 01:01:52 +0200

    fix bug

--- m_API_cmc.py
+++ m_API_cmc.py
@@ -21,76 +21,76 @@ def get_last_cmc(dbconnect, headers, refresh=60):
     """, refresh)
     get_update_datetime_by_id = cursor.fetchall()
     print(get_update_datetime_by_id )
-    if get_update_datetime_by_id == [] :
-        print("Wallet is empty")
-    else:
+    #if get_update_datetime_by_id == [] :
+    #    pass
+    
+    #cursor.close()#BUG
+    print("test 1 get_last_cmc")#TODO delete
+    for i in get_update_datetime_by_id:
+        print("test 2 get_last_cmc, update of the id", i)#TODO delete
+        session = Session()
+        session.headers.update(headers)
+        url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
+        convert_id = '2790' #2790 code pour convertir en euros
+        parameters = {
+            'id':i[0],
+            'convert_id':convert_id
+        }
+        response = session.get(url, params=parameters)
+        actual_value = (json.loads(response.text)['data'][str(i[0])]) # get dictionnary for id
+        percent_change_24h = actual_value['quote'][str(convert_id)]['percent_change_24h']
+        percent_change_7d = actual_value['quote'][str(convert_id)]['percent_change_7d']
+        price = actual_value['quote'][str(convert_id)]['price']
+        symbol = actual_value['symbol']
+        name = actual_value['name']
+        if percent_change_24h > 0:
+            tendancy_24h = "./static/pictures/graph_up.PNG"
+        else:
+            tendancy_24h = "./static/pictures/graph_down.PNG"
+        if percent_change_7d > 0:
+            tendancy_7d = "./static/pictures/graph_up.PNG"
+        else:
+            tendancy_7d = "./static/pictures/graph_down.PNG"
+        
+        # Save into actual_datas table
+        #date = datetime.now()
+        datas = (price,percent_change_24h,percent_change_7d,tendancy_24h,tendancy_7d,symbol,name,i[0]) #Try date replaced by last_updated
+        #cursor = dbconnect.cursor() #BUG
+        cursor.execute("""
+            UPDATE actual_datas
+            SET 
+                actual_value = %s, 
+                update_date = UTC_DATE(),
+                update_date_time = UTC_TIMESTAMP(),
+                percent_change_24h = %s,
+                percent_change_7d = %s,
+                tendancy_24h = %s,
+                tendancy_7d = %s,
+                symbol = %s,
+                name = %s
+            WHERE id_crypto = %s
+            """, datas)
+        dbconnect.commit()
         #cursor.close()#BUG
-        print("test 1 get_last_cmc")#TODO delete
-        for i in get_update_datetime_by_id:
-            print("test 2 get_last_cmc, update of the id", i)#TODO delete
-            session = Session()
-            session.headers.update(headers)
-            url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
-            convert_id = '2790' #2790 code pour convertir en euros
-            parameters = {
-                'id':i[0],
-                'convert_id':convert_id
-            }
-            response = session.get(url, params=parameters)
-            actual_value = (json.loads(response.text)['data'][str(i[0])]) # get dictionnary for id
-            percent_change_24h = actual_value['quote'][str(convert_id)]['percent_change_24h']
-            percent_change_7d = actual_value['quote'][str(convert_id)]['percent_change_7d']
-            price = actual_value['quote'][str(convert_id)]['price']
-            symbol = actual_value['symbol']
-            name = actual_value['name']
-            if percent_change_24h > 0:
-                tendancy_24h = "./static/pictures/graph_up.PNG"
-            else:
-                tendancy_24h = "./static/pictures/graph_down.PNG"
-            if percent_change_7d > 0:
-                tendancy_7d = "./static/pictures/graph_up.PNG"
-            else:
-                tendancy_7d = "./static/pictures/graph_down.PNG"
-            
-            # Save into actual_datas table
-            #date = datetime.now()
-            datas = (price,percent_change_24h,percent_change_7d,tendancy_24h,tendancy_7d,symbol,name,i[0]) #Try date replaced by last_updated
-            #cursor = dbconnect.cursor() #BUG
-            cursor.execute("""
-                UPDATE actual_datas
-                SET 
-                    actual_value = %s, 
-                    update_date = UTC_DATE(),
-                    update_date_time = UTC_TIMESTAMP(),
-                    percent_change_24h = %s,
-                    percent_change_7d = %s,
-                    tendancy_24h = %s,
-                    tendancy_7d = %s,
-                    symbol = %s,
-                    name = %s
-                WHERE id_crypto = %s
-                """, datas)
-            dbconnect.commit()
-            #cursor.close()#BUG
 
-            # Step 4 : Get the crypto logo & save it in the database
-            url_logo = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info'
-            parameters_logo = {
-                'id':i[0]
-            }
-            response_logo = session.get(url_logo, params=parameters_logo)
-            logo_link = json.loads(response_logo.text)['data'][str(i[0])]['logo'] #recupération du logo
-            datas = (logo_link,i[0])
-            # Save logo into actual_datas table
-            #cursor = dbconnect.cursor() #BUG
-            cursor.execute("""
-                UPDATE actual_datas
-                SET logo =  %s
-                WHERE id_crypto = %s;
-                """,datas)
-            dbconnect.commit()
-            print("id_",i[0]," updated")
-        cursor.close()#BUG
+        # Step 4 : Get the crypto logo & save it in the database
+        url_logo = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/info'
+        parameters_logo = {
+            'id':i[0]
+        }
+        response_logo = session.get(url_logo, params=parameters_logo)
+        logo_link = json.loads(response_logo.text)['data'][str(i[0])]['logo'] #recupération du logo
+        datas = (logo_link,i[0])
+        # Save logo into actual_datas table
+        #cursor = dbconnect.cursor() #BUG
+        cursor.execute("""
+            UPDATE actual_datas
+            SET logo =  %s
+            WHERE id_crypto = %s;
+            """,datas)
+        dbconnect.commit()
+        print("id_",i[0]," updated")
+    cursor.close()#BUG
 
 def get_crypto_list(headers, limit=100):
     
